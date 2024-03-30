a0b10639c4867954d4a820b939545fac8437500d
JLouisse088 <louissepolong890@gmail.com>
2024-03-30 10:37:27 +0800

    initial commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+
+// PascalCasing -> every letter is capitalize
+function Message() {
+    const name = "John";
+    // JSX (JavaScript XML) -> it use HTML code inside of JavaScript code
+    return <h1>Hello {name}!</h1>;
+}
+
+export default Message; // export -> to import different file,  default -> one file only, without default -> multiple files
